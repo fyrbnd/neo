8ac98e536b1dc4aca4413486be52f13fc8722ab3
fyrbnd <148682661+fyrbnd@users.noreply.github.com>
2023-10-26 19:12:24 +0200

    Update neo.h

--- neo.h
+++ neo.h
@@ -1,6 +1,27 @@
 #ifndef NEO_GRAPH_H_
 #define NEO_GRAPH_H_
 
+
+/*===------------ neo - Library for Working With Graphs in C ------------===*\
+|*                                                                          *|
+|* This file is part of Neo.                                                *|
+|*                                                                          *|
+|* Neo is free software: you can redistribute it and/or modify it under the *|
+|* terms of the GNU Lesser General Public License as published by the Free  *|
+|* Software Foundation, version 2.1 of the License, or (at your option) any *|
+|* later version.                                                           *|
+|*                                                                          *|
+|* Neo is distributed in the hope that it will be useful, but WITHOUT ANY   *|
+|* WARRANTY; without even the implied warranty of MERCHANTABILITY or        *|
+|* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public      *|
+|* License for more details.                                                *|
+|*                                                                          *|
+|* You should have received a copy of the GNU Lesser General Public License *|
+|* along with Neo. If not, see <https://www.gnu.org/licenses/>.             *|
+|*                                                                          *|
+\*===--------------------------------------------------------------------===*/
+
+
 typedef unsigned char uchar;
 typedef unsigned int uint;
 typedef unsigned long ulong;
